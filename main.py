92cb41466f341be8164a3dd38c1c3ce590df9f86
SubegS <subegsinghkapoor360@gmail.com>
2025-12-24 13:12:29 +0530

    Invoking the Graph

--- main.py
+++ main.py
@@ -44,4 +44,24 @@ builder.add_edge(REFLECT, GENERATE)
 
 graph = builder.compile()
 print(graph.get_graph().draw_mermaid())
-graph.get_graph().print_ascii()
+#graph.get_graph().print_ascii()
+
+if __name__ == "__main__":
+    print("Hello LangGraph")
+    inputs = {
+        "messages": [
+            HumanMessage(
+                content="""Make this tweet better:"
+                                    @LangChainAI
+            — newly Tool Calling feature is seriously underrated.
+
+            After a long wait, it's  here- making the implementation of agents across different models with function calling - super easy.
+
+            Made a video covering their newest blog post
+
+                                  """
+            )
+        ]
+    }
+    response = graph.invoke(inputs)
+    print(response)
